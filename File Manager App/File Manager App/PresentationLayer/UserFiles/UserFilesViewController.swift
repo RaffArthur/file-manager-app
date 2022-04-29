c08e4a4f39591d687c80c7422fdd8b49f5a3ad50
Arthur Raff <arthur.raff@icloud.com>
2022-04-29 18:00:18 +0300

    SceneDelegate prep work completed

---
+++ File Manager App/File Manager App/PresentationLayer/UserFiles/UserFilesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserFilesViewController.swift
+//  File Manager App
+//
+//  Created by Arthur Raff on 29.04.2022.
+//
+
+import UIKit
+
+import SnapKit
+
+class UserFilesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+}
