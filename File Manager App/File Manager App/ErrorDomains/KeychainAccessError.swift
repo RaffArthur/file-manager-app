9a3dd6ba2b70e61b0efba678953e929ecf8849e9
Arthur Raff <arthur.raff@icloud.com>
2022-05-03 03:28:01 +0300

    KeychainAccessError added

---
+++ File Manager App/File Manager App/ErrorDomains/KeychainAccessError.swift
@@ -0,0 +1,15 @@
+//
+//  KeychainAccessError.swift
+//  File Manager App
+//
+//  Created by Arthur Raff on 02.05.2022.
+//
+
+import Foundation
+
+enum KeychainAccessError: Error {
+    case wrongPassword
+    case weakPassword
+    case userAlreadyExist
+    case unknownError
+}
