4f8bf21a4bad957bcb4ac21b8a801b7f7287ead9
Arthur Raff <arthur.raff@icloud.com>
2022-05-14 16:03:48 +0300

    SceneDelegate root view controller implementation edited

--- File Manager App/File Manager App/ApplicationLayer/SceneDelegate.swift
+++ File Manager App/File Manager App/ApplicationLayer/SceneDelegate.swift
@@ -9,35 +9,20 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
     
         window = UIWindow(windowScene: windowScene)
         
-        let rootViewController = UINavigationController(rootViewController: RegistrationViewController())
+        let navigationController: UINavigationController = .init()
         
-        window?.rootViewController = rootViewController
+        appCoordinator = AppCoordinator(navigationController)
+        
+        appCoordinator?.start()
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        
     }
 }
